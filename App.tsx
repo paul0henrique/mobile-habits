3afc0202978959fa58207c12c6ae59ff709e56b8
Paulo <paulohenriquesms10@gmail.com>
2023-01-17 12:33:28 -0300

    add: setup mobile

---
+++ App.tsx
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { 
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter'
+
+import { Loading } from './src/assets/components/Loading';
+
+export default function App() {
+  const [fontsLoadead] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoadead){
+    return (
+      <Loading />
+    );
+  }
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#09090A",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  text: {
+    color: "#FFF",
+    fontFamily: 'Inter_800ExtraBold'
+  }
+});
